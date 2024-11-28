90c7ec956ecc0f4e814bcf296a261ff1aa5bb411
kusuma469 <kusuma040620@gmail.com>
2024-11-28 16:49:02 -0500

    deployment- changes

--- middleware.ts
+++ middleware.ts
@@ -10,9 +10,13 @@ export default clerkMiddleware((auth, request) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+
+    // '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // '/(api|trpc)(.*)',
+  
+    // Match all routes except static files
+    '/((?!_next/static|favicon.ico|_next/image|.*\\.(css|js|png|jpg|jpeg|svg|ico|webp|ttf|woff2)).*)',
     '/(api|trpc)(.*)',
+    // '/api/(.*)', // Always run for API routes
   ],
 };
