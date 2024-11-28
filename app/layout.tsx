1fb0a879d1b01f71edf6645296f9c83e951ccbba
kusuma469 <kusuma040620@gmail.com>
2024-11-28 16:16:26 -0500

    deployment changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import { ClerkProvider, SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
 import './globals.css';
 import { Toaster } from 'sonner';
 import { ConvexClientProvider } from '@/providers/convex-client-provider'; // Import ConvexProvider
@@ -7,7 +7,7 @@ import { ModalProvider } from '@/providers/modal-provider';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     
-      
+      <ClerkProvider>
         <html lang="en">
           <body>
             <header>
@@ -26,6 +26,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </header>
           </body>
         </html>
+      </ClerkProvider>
+
       
     
   );
